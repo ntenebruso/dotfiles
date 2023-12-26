9427048e1b719399b82df05c564dc045814d950a
Nick Tenebruso <nickten2006@gmail.com>
2023-12-26 17:23:35 -0500

    Update hyprland keybinds

--- .config/hypr/scripts/autostart.sh
+++ .config/hypr/scripts/autostart.sh
@@ -1,6 +1,7 @@
 swayidle -w \
-	timeout 300 "swaylock -f" \
 	timeout 300 "hyprctl dispatch dpms off" \
 		resume "hyprctl dispatch dpms on" \
+	timeout 315 "swaylock -f" \
+	timeout 600 "systemctl suspend" \
 	before-sleep "swaylock -f" &
 
