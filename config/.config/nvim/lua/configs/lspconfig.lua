68b8c968e9df541f54a33eb7743e011d239b4079
Nick Tenebruso <nickten2006@gmail.com>
2024-09-07 23:31:03 -0400

    Add python to lsp config

--- config/.config/nvim/lua/configs/lspconfig.lua
+++ config/.config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ts_ls", "astro" }
+local servers = { "html", "cssls", "ts_ls", "astro", "pylsp" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
